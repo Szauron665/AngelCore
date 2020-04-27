allankrdec/TrinityCore-10-2024

Core/Quest: fix RewardNextQuest being used to condition previous quests in chain

src/server/game/Globals/ObjectMgr.cpp
@@ -4579,8 +4579,6 @@ void ObjectMgr::LoadQuests()
                     qinfo->GetQuestId(), qinfo->_rewardNextQuest, qinfo->_rewardNextQuest);
                 qinfo->_rewardNextQuest = 0;
             }
-            else
-                qNextItr->second->PrevChainQuests.push_back(qinfo->GetQuestId());
         }
 
         // fill additional data stores
@@ -4592,9 +4590,9 @@ void ObjectMgr::LoadQuests()
 
         if (qinfo->_nextQuestId)
         {
-            auto qNextItr = _questTemplates.find(qinfo->GetNextQuestId());
+            auto qNextItr = _questTemplates.find(qinfo->_nextQuestId);
             if (qNextItr == _questTemplates.end())
-                TC_LOG_ERROR("sql.sql", "Quest %u has NextQuestId %u, but no such quest", qinfo->GetQuestId(), qinfo->GetNextQuestId());
+                TC_LOG_ERROR("sql.sql", "Quest %u has NextQuestId %u, but no such quest", qinfo->GetQuestId(), qinfo->_nextQuestId);
             else
                 qNextItr->second->DependentPreviousQuests.push_back(qinfo->GetQuestId());
         }

Szauron665/AngelCore

(cherry picked from commit 214b8e53c5598695915c1392702e059d79cfc260)

src/server/game/Globals/ObjectMgr.cpp
@@ -4529,8 +4529,6 @@ void ObjectMgr::LoadQuests()
                     qinfo->GetQuestId(), qinfo->_nextQuestInChain, qinfo->_nextQuestInChain);
                 qinfo->_nextQuestInChain = 0;
             }
-            else
-                qNextItr->second->PrevChainQuests.push_back(qinfo->GetQuestId());
         }
 
         for (uint8 j = 0; j < QUEST_REWARD_CURRENCY_COUNT; ++j)
@@ -4617,9 +4615,9 @@ void ObjectMgr::LoadQuests()
 
         if (qinfo->_nextQuestID)
         {
-            auto qNextItr = _questTemplates.find(qinfo->GetNextQuestId());
+            auto qNextItr = _questTemplates.find(qinfo->_nextQuestID);
             if (qNextItr == _questTemplates.end())
-                TC_LOG_ERROR("sql.sql", "Quest %d has NextQuestId %u, but no such quest", qinfo->GetQuestId(), qinfo->GetNextQuestId());
+                TC_LOG_ERROR("sql.sql", "Quest %d has NextQuestId %u, but no such quest", qinfo->GetQuestId(), qinfo->_nextQuestID);
             else
                 qNextItr->second->DependentPreviousQuests.push_back(qinfo->GetQuestId());
         }