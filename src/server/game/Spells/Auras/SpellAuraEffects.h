allankrdec/TrinityCore-10-2024

Core/Auras: periodics refactor part 1:
- Don't skip ticks if diff is greater than amplitude
- Fixed spells without ampltude in DBC

src/server/game/Spells/Auras/SpellAuraEffects.h
@@ -50,7 +50,7 @@ class TC_GAME_API AuraEffect
         uint32 GetId() const { return m_spellInfo->Id; }
         uint32 GetEffIndex() const { return m_effIndex; }
         int32 GetBaseAmount() const { return m_baseAmount; }
-        int32 GetAmplitude() const { return m_amplitude; }
+        int32 GetAmplitude() const { return _amplitude; }
 
         int32 GetMiscValueB() const { return m_spellInfo->Effects[m_effIndex].MiscValueB; }
         int32 GetMiscValue() const { return m_spellInfo->Effects[m_effIndex].MiscValue; }
@@ -58,8 +58,8 @@ class TC_GAME_API AuraEffect
         int32 GetAmount() const { return m_amount; }
         void SetAmount(int32 amount) { m_amount = amount; m_canBeRecalculated = false;}
 
-        int32 GetPeriodicTimer() const { return m_periodicTimer; }
-        void SetPeriodicTimer(int32 periodicTimer) { m_periodicTimer = periodicTimer; }
+        int32 GetPeriodicTimer() const { return _periodicTimer; }
+        void SetPeriodicTimer(int32 periodicTimer) { _periodicTimer = periodicTimer; }
 
         int32 CalculateAmount(Unit* caster);
         void CalculatePeriodic(Unit* caster, bool resetPeriodicTimer = true, bool load = false);
@@ -83,9 +83,11 @@ class TC_GAME_API AuraEffect
         void Update(uint32 diff, Unit* caster);
         void UpdatePeriodic(Unit* caster);
 
-        uint32 GetTickNumber() const { return m_tickNumber; }
-        int32 GetTotalTicks() const { return m_amplitude ? (GetBase()->GetMaxDuration() / m_amplitude) : 1;}
-        void ResetPeriodic(bool resetPeriodicTimer = false) { if (resetPeriodicTimer) m_periodicTimer = m_amplitude; m_tickNumber = 0;}
+        void ResetTicks() { _ticksDone = 0; }
+        uint32 GetTickNumber() const { return _ticksDone; }
+        uint32 GetRemainingTicks() const { return GetTotalTicks() - _ticksDone; }
+        uint32 GetTotalTicks() const { return (_amplitude && !GetBase()->IsPermanent()) ? uint32(GetBase()->GetMaxDuration() / _amplitude) : uint32(0); }
+        void ResetPeriodic(bool resetPeriodicTimer = false);
 
         bool IsPeriodic() const { return m_isPeriodic; }
         void SetPeriodic(bool isPeriodic) { m_isPeriodic = isPeriodic; }
@@ -115,9 +117,10 @@ class TC_GAME_API AuraEffect
 
         SpellModifier* m_spellmod;
 
-        int32 m_periodicTimer;
-        int32 m_amplitude;
-        uint32 m_tickNumber;
+        // periodic stuff
+        int32 _periodicTimer;
+        int32 _amplitude;       // time between consecutive ticks
+        uint32 _ticksDone;      // ticks counter
 
         uint8 const m_effIndex;
         bool m_canBeRecalculated;

Szauron665/AngelCore

(cherry picked from commit 5d91beb1dbc795382a12ec05f6e72c76b195b141)

src/server/game/Spells/Auras/SpellAuraEffects.h
@@ -49,7 +49,7 @@ class TC_GAME_API AuraEffect
         uint32 GetId() const { return m_spellInfo->Id; }
         uint32 GetEffIndex() const { return m_effectInfo->EffectIndex; }
         int32 GetBaseAmount() const { return m_baseAmount; }
-        int32 GetPeriod() const { return m_period; }
+        int32 GetPeriod() const { return _period; }
 
         int32 GetMiscValueB() const { return GetSpellEffectInfo()->MiscValueB; }
         int32 GetMiscValue() const { return GetSpellEffectInfo()->MiscValue; }
@@ -57,8 +57,8 @@ class TC_GAME_API AuraEffect
         int32 GetAmount() const { return m_amount; }
         void SetAmount(int32 amount) { m_amount = amount; m_canBeRecalculated = false;}
 
-        int32 GetPeriodicTimer() const { return m_periodicTimer; }
-        void SetPeriodicTimer(int32 periodicTimer) { m_periodicTimer = periodicTimer; }
+        int32 GetPeriodicTimer() const { return _periodicTimer; }
+        void SetPeriodicTimer(int32 periodicTimer) { _periodicTimer = periodicTimer; }
 
         int32 CalculateAmount(Unit* caster);
         void CalculatePeriodic(Unit* caster, bool resetPeriodicTimer = true, bool load = false);
@@ -82,9 +82,11 @@ class TC_GAME_API AuraEffect
         void Update(uint32 diff, Unit* caster);
         void UpdatePeriodic(Unit* caster);
 
-        uint32 GetTickNumber() const { return m_tickNumber; }
-        int32 GetTotalTicks() const { return m_period ? (GetBase()->GetMaxDuration() / m_period) : 1;}
-        void ResetPeriodic(bool resetPeriodicTimer = false) { if (resetPeriodicTimer) m_periodicTimer = m_period; m_tickNumber = 0;}
+        void ResetTicks() { _ticksDone = 0; }
+        uint32 GetTickNumber() const { return _ticksDone; }
+        uint32 GetRemainingTicks() const { return GetTotalTicks() - _ticksDone; }
+        uint32 GetTotalTicks() const { return (_period && !GetBase()->IsPermanent()) ? uint32(GetBase()->GetMaxDuration() / _period) : uint32(0); }
+        void ResetPeriodic(bool resetPeriodicTimer = false);
 
         bool IsPeriodic() const { return m_isPeriodic; }
         void SetPeriodic(bool isPeriodic) { m_isPeriodic = isPeriodic; }
@@ -120,9 +122,10 @@ class TC_GAME_API AuraEffect
         float m_critChance;
         float m_donePct;
 
-        int32 m_periodicTimer;
-        int32 m_period;
-        uint32 m_tickNumber;
+        // periodic stuff
+        int32 _periodicTimer;
+        int32 _period;          // time between consecutive ticks
+        uint32 _ticksDone;      // ticks counter
 
         bool m_canBeRecalculated;
         bool m_isPeriodic;