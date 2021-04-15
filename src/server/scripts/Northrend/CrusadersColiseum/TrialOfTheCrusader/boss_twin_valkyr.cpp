allankrdec/TrinityCore-10-2024

Scripts/Trial of Crusader: Northrend Beasts Rework (#21031)
* Rewritten Gormok encounter
* Rewritten Snobold Vassal Script
* Rewritten Jormungars encounter
* Rewritten Icehowl encounter
* Added missing texts
* Added missing spells
* Implemented berserk mechanic on heroic diffs.
* Implemented Achievement requirement to make heroic diffs.
* Rewritten Barrett Ramsay scripts
* Added Barrett gossips and ported everything to DB.
* Added right Barrett in each encounter
* Removed a lot of hacks
* Rewritten Tirion Fordring script
* Rewritten Varian Wrynn script
* Rewritten Garrosh script
* Rewritten Wilfred event
* Rewritten Lich King event
* Updated codestyle
* Added missing spawns
* Fixed combat behavior before Gormok
* Fixed some visual stuff

src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_twin_valkyr.cpp
@@ -46,10 +46,14 @@ enum Texts
     SAY_DEATH               = 8
 };
 
-enum Equipment
+enum Misc
 {
-    EQUIP_MAIN_1         = 9423,
-    EQUIP_MAIN_2         = 37377
+    EQUIP_MAIN_1            = 9423,
+    EQUIP_MAIN_2            = 37377,
+    POINT_INITIAL_MOVEMENT  = 1,
+    SPLINE_INITIAL_MOVEMENT = 1,
+    PHASE_EVENT             = 1,
+    PHASE_COMBAT            = 2
 };
 
 enum Summons
@@ -101,7 +105,8 @@ enum Events
     EVENT_TWIN_SPIKE      = 1,
     EVENT_TOUCH           = 2,
     EVENT_SPECIAL_ABILITY = 3,
-    EVENT_BERSERK         = 4
+    EVENT_BERSERK         = 4,
+    EVENT_START_MOVE      = 5
 };
 
 enum Stages
@@ -133,9 +138,7 @@ enum Actions
 class OrbsDespawner : public BasicEvent
 {
     public:
-        explicit OrbsDespawner(Creature* creature) : _creature(creature)
-        {
-        }
+        explicit OrbsDespawner(Creature* creature) : _creature(creature) { }
 
         bool Execute(uint64 /*currTime*/, uint32 /*diff*/) override
         {
@@ -186,7 +189,6 @@ struct boss_twin_baseAI : public BossAI
 
     void Reset() override
     {
-        me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE | UNIT_FLAG_NOT_SELECTABLE);
         me->SetReactState(REACT_PASSIVE);
         me->ModifyAuraState(AuraState, true);
         /* Uncomment this once that they are floating above the ground
@@ -194,30 +196,26 @@ struct boss_twin_baseAI : public BossAI
         me->SetFlying(true); */
 
         summons.DespawnAll();
+        events.SetPhase(PHASE_EVENT);
+        events.ScheduleEvent(EVENT_START_MOVE, 4s);
     }
 
     void JustReachedHome() override
     {
         instance->SetBossState(DATA_TWIN_VALKIRIES, FAIL);
-
+        HandleRemoveAuras();
         summons.DespawnAll();
         me->DespawnOrUnsummon();
     }
 
-    void MovementInform(uint32 uiType, uint32 uiId) override
+    void MovementInform(uint32 type, uint32 pointId) override
     {
-        if (uiType != POINT_MOTION_TYPE)
-            return;
-
-        switch (uiId)
+        if (type == SPLINE_CHAIN_MOTION_TYPE && pointId == POINT_INITIAL_MOVEMENT)
         {
-            case 1:
-                me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE | UNIT_FLAG_NOT_SELECTABLE);
-                me->SetImmuneToPC(false);
-                me->SetReactState(REACT_AGGRESSIVE);
-                break;
-            default:
-                break;
+            me->SetImmuneToPC(false);
+            me->SetReactState(REACT_AGGRESSIVE);
+            if (me->GetEntry() == NPC_FJOLA_LIGHTBANE) // avoid call twice
+                instance->DoCloseDoorOrButton(instance->GetGuidData(DATA_MAIN_GATE));
         }
     }
 
@@ -227,25 +225,13 @@ struct boss_twin_baseAI : public BossAI
             Talk(SAY_KILL_PLAYER);
     }
 
-    void SummonedCreatureDespawn(Creature* summoned) override
+    void HandleRemoveAuras()
     {
-        switch (summoned->GetEntry())
-        {
-            case NPC_LIGHT_ESSENCE:
-                instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_LIGHT_ESSENCE_HELPER);
-                instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_POWERING_UP_HELPER);
-                break;
-            case NPC_DARK_ESSENCE:
-                instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_DARK_ESSENCE_HELPER);
-                instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_POWERING_UP_HELPER);
-                break;
-            case NPC_BULLET_CONTROLLER:
-                me->m_Events.AddEvent(new OrbsDespawner(me), me->m_Events.CalculateTime(100));
-                break;
-            default:
-                break;
-        }
-        summons.Despawn(summoned);
+        instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_LIGHT_ESSENCE_HELPER);
+        instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_POWERING_UP_HELPER);
+        instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_DARK_ESSENCE_HELPER);
+        instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_POWERING_UP_HELPER);
+        me->m_Events.AddEventAtOffset(new OrbsDespawner(me), 100ms);
     }
 
     void JustDied(Unit* /*killer*/) override
@@ -259,6 +245,7 @@ struct boss_twin_baseAI : public BossAI
                 pSister->SetFlag(UNIT_DYNAMIC_FLAGS, UNIT_DYNFLAG_LOOTABLE);
                 events.Reset();
                 summons.DespawnAll();
+                HandleRemoveAuras();
                 instance->SetBossState(DATA_TWIN_VALKIRIES, DONE);
             }
             else
@@ -341,9 +328,33 @@ struct boss_twin_baseAI : public BossAI
                 DoCast(me, SPELL_BERSERK);
                 Talk(SAY_BERSERK);
                 break;
+            case EVENT_START_MOVE:
+                events.SetPhase(PHASE_COMBAT);
+                me->GetMotionMaster()->MoveAlongSplineChain(POINT_INITIAL_MOVEMENT, SPLINE_INITIAL_MOVEMENT, false);
+                break;
             default:
                 break;
         }
+    }
+
+    void UpdateAI(uint32 diff) override
+    {
+        if (!UpdateVictim() && !events.IsInPhase(PHASE_EVENT))
+            return;
+
+        events.Update(diff);
+
+        if (me->HasUnitState(UNIT_STATE_CASTING))
+            return;
+
+        while (uint32 eventId = events.ExecuteEvent())
+        {
+            ExecuteEvent(eventId);
+            if (me->HasUnitState(UNIT_STATE_CASTING))
+                return;
+        }
+
+        DoMeleeAttackIfReady();
     }
 
     protected:

Szauron665/AngelCore

(cherry picked from commit 2362e9c79410c689afc10266b94f7e78c88bbddc)

src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_twin_valkyr.cpp
@@ -44,10 +44,14 @@ enum Texts
     SAY_DEATH               = 8
 };
 
-enum Equipment
+enum Misc
 {
-    EQUIP_MAIN_1         = 9423,
-    EQUIP_MAIN_2         = 37377
+    EQUIP_MAIN_1            = 9423,
+    EQUIP_MAIN_2            = 37377,
+    POINT_INITIAL_MOVEMENT  = 1,
+    SPLINE_INITIAL_MOVEMENT = 1,
+    PHASE_EVENT             = 1,
+    PHASE_COMBAT            = 2
 };
 
 enum Summons
@@ -100,7 +104,8 @@ enum Events
     EVENT_TWIN_SPIKE      = 1,
     EVENT_TOUCH           = 2,
     EVENT_SPECIAL_ABILITY = 3,
-    EVENT_BERSERK         = 4
+    EVENT_BERSERK         = 4,
+    EVENT_START_MOVE      = 5
 };
 
 enum Stages
@@ -124,9 +129,7 @@ enum Actions
 class OrbsDespawner : public BasicEvent
 {
     public:
-        explicit OrbsDespawner(Creature* creature) : _creature(creature)
-        {
-        }
+        explicit OrbsDespawner(Creature* creature) : _creature(creature) { }
 
         bool Execute(uint64 /*currTime*/, uint32 /*diff*/) override
         {
@@ -177,7 +180,6 @@ struct boss_twin_baseAI : public BossAI
 
     void Reset() override
     {
-        me->AddUnitFlag(UnitFlags(UNIT_FLAG_NON_ATTACKABLE | UNIT_FLAG_NOT_SELECTABLE));
         me->SetReactState(REACT_PASSIVE);
         me->ModifyAuraState(AuraState, true);
         /* Uncomment this once that they are floating above the ground
@@ -185,30 +187,26 @@ struct boss_twin_baseAI : public BossAI
         me->SetFlying(true); */
 
         summons.DespawnAll();
+        events.SetPhase(PHASE_EVENT);
+        events.ScheduleEvent(EVENT_START_MOVE, 4s);
     }
 
     void JustReachedHome() override
     {
         instance->SetBossState(DATA_TWIN_VALKIRIES, FAIL);
-
+        HandleRemoveAuras();
         summons.DespawnAll();
         me->DespawnOrUnsummon();
     }
 
-    void MovementInform(uint32 uiType, uint32 uiId) override
+    void MovementInform(uint32 type, uint32 pointId) override
     {
-        if (uiType != POINT_MOTION_TYPE)
-            return;
-
-        switch (uiId)
+        if (type == SPLINE_CHAIN_MOTION_TYPE && pointId == POINT_INITIAL_MOVEMENT)
         {
-            case 1:
-                me->RemoveUnitFlag(UnitFlags(UNIT_FLAG_NON_ATTACKABLE | UNIT_FLAG_NOT_SELECTABLE));
-                me->SetImmuneToPC(false);
-                me->SetReactState(REACT_AGGRESSIVE);
-                break;
-            default:
-                break;
+            me->SetImmuneToPC(false);
+            me->SetReactState(REACT_AGGRESSIVE);
+            if (me->GetEntry() == NPC_FJOLA_LIGHTBANE) // avoid call twice
+                instance->DoCloseDoorOrButton(instance->GetGuidData(DATA_MAIN_GATE));
         }
     }
 
@@ -218,25 +216,13 @@ struct boss_twin_baseAI : public BossAI
             Talk(SAY_KILL_PLAYER);
     }
 
-    void SummonedCreatureDespawn(Creature* summoned) override
+    void HandleRemoveAuras()
     {
-        switch (summoned->GetEntry())
-        {
-            case NPC_LIGHT_ESSENCE:
-                instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_LIGHT_ESSENCE);
-                instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_POWERING_UP);
-                break;
-            case NPC_DARK_ESSENCE:
-                instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_DARK_ESSENCE);
-                instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_POWERING_UP);
-                break;
-            case NPC_BULLET_CONTROLLER:
-                me->m_Events.AddEvent(new OrbsDespawner(me), me->m_Events.CalculateTime(100));
-                break;
-            default:
-                break;
-        }
-        summons.Despawn(summoned);
+        instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_LIGHT_ESSENCE);
+        instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_POWERING_UP);
+        instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_DARK_ESSENCE);
+        instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_POWERING_UP);
+        me->m_Events.AddEventAtOffset(new OrbsDespawner(me), 100ms);
     }
 
     void JustDied(Unit* /*killer*/) override
@@ -250,6 +236,7 @@ struct boss_twin_baseAI : public BossAI
                 pSister->AddDynamicFlag(UNIT_DYNFLAG_LOOTABLE);
                 events.Reset();
                 summons.DespawnAll();
+                HandleRemoveAuras();
                 instance->SetBossState(DATA_TWIN_VALKIRIES, DONE);
             }
             else
@@ -332,9 +319,33 @@ struct boss_twin_baseAI : public BossAI
                 DoCast(me, SPELL_BERSERK);
                 Talk(SAY_BERSERK);
                 break;
+            case EVENT_START_MOVE:
+                events.SetPhase(PHASE_COMBAT);
+                me->GetMotionMaster()->MoveAlongSplineChain(POINT_INITIAL_MOVEMENT, SPLINE_INITIAL_MOVEMENT, false);
+                break;
             default:
                 break;
         }
+    }
+
+    void UpdateAI(uint32 diff) override
+    {
+        if (!UpdateVictim() && !events.IsInPhase(PHASE_EVENT))
+            return;
+
+        events.Update(diff);
+
+        if (me->HasUnitState(UNIT_STATE_CASTING))
+            return;
+
+        while (uint32 eventId = events.ExecuteEvent())
+        {
+            ExecuteEvent(eventId);
+            if (me->HasUnitState(UNIT_STATE_CASTING))
+                return;
+        }
+
+        DoMeleeAttackIfReady();
     }
 
     protected: