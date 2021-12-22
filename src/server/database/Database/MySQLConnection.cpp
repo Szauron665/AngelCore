allankrdec/TrinityCore-10-2024

Core/DBLayer: Refactor PreparedStatement class to not depend on MySQLPreparedStatement

src/server/database/Database/MySQLConnection.cpp
@@ -198,13 +198,12 @@ bool MySQLConnection::Execute(PreparedStatement* stmt)
     if (!m_Mysql)
         return false;
 
-    uint32 index = stmt->m_index;
+    uint32 index = stmt->GetIndex();
 
     MySQLPreparedStatement* m_mStmt = GetPreparedStatement(index);
     ASSERT(m_mStmt);            // Can only be null if preparation failed, server side error or bad query
-    m_mStmt->m_stmt = stmt;     // Cross reference them for debug output
 
-    stmt->BindParameters(m_mStmt);
+    m_mStmt->BindParameters(stmt);
 
     MYSQL_STMT* msql_STMT = m_mStmt->GetSTMT();
     MYSQL_BIND* msql_BIND = m_mStmt->GetBind();
@@ -241,18 +240,18 @@ bool MySQLConnection::Execute(PreparedStatement* stmt)
     return true;
 }
 
-bool MySQLConnection::_Query(PreparedStatement* stmt, MySQLResult** pResult, uint64* pRowCount, uint32* pFieldCount)
+bool MySQLConnection::_Query(PreparedStatement* stmt, MySQLPreparedStatement** mysqlStmt, MySQLResult** pResult, uint64* pRowCount, uint32* pFieldCount)
 {
     if (!m_Mysql)
         return false;
 
-    uint32 index = stmt->m_index;
+    uint32 index = stmt->GetIndex();
 
     MySQLPreparedStatement* m_mStmt = GetPreparedStatement(index);
     ASSERT(m_mStmt);            // Can only be null if preparation failed, server side error or bad query
-    m_mStmt->m_stmt = stmt;     // Cross reference them for debug output
 
-    stmt->BindParameters(m_mStmt);
+    m_mStmt->BindParameters(stmt);
+    *mysqlStmt = m_mStmt;
 
     MYSQL_STMT* msql_STMT = m_mStmt->GetSTMT();
     MYSQL_BIND* msql_BIND = m_mStmt->GetBind();
@@ -265,7 +264,7 @@ bool MySQLConnection::_Query(PreparedStatement* stmt, MySQLResult** pResult, uin
         TC_LOG_ERROR("sql.sql", "SQL(p): %s\n [ERROR]: [%u] %s", m_mStmt->getQueryString().c_str(), lErrno, mysql_stmt_error(msql_STMT));
 
         if (_HandleMySQLErrno(lErrno))  // If it returns true, an error was handled successfully (i.e. reconnection)
-            return _Query(stmt, pResult, pRowCount, pFieldCount);       // Try again
+            return _Query(stmt, mysqlStmt, pResult, pRowCount, pFieldCount);       // Try again
 
         m_mStmt->ClearParameters();
         return false;
@@ -278,7 +277,7 @@ bool MySQLConnection::_Query(PreparedStatement* stmt, MySQLResult** pResult, uin
             m_mStmt->getQueryString().c_str(), lErrno, mysql_stmt_error(msql_STMT));
 
         if (_HandleMySQLErrno(lErrno))  // If it returns true, an error was handled successfully (i.e. reconnection)
-            return _Query(stmt, pResult, pRowCount, pFieldCount);      // Try again
+            return _Query(stmt, mysqlStmt, pResult, pRowCount, pFieldCount);      // Try again
 
         m_mStmt->ClearParameters();
         return false;
@@ -494,18 +493,19 @@ void MySQLConnection::PrepareStatement(uint32 index, std::string const& sql, Con
 
 PreparedResultSet* MySQLConnection::Query(PreparedStatement* stmt)
 {
-    MySQLResult* result = NULL;
+    MySQLPreparedStatement* mysqlStmt = nullptr;
+    MySQLResult* result = nullptr;
     uint64 rowCount = 0;
     uint32 fieldCount = 0;
 
-    if (!_Query(stmt, &result, &rowCount, &fieldCount))
+    if (!_Query(stmt, &mysqlStmt, &result, &rowCount, &fieldCount))
         return nullptr;
 
     if (mysql_more_results(m_Mysql))
     {
         mysql_next_result(m_Mysql);
     }
-    return new PreparedResultSet(stmt->m_stmt->GetSTMT(), result, rowCount, fieldCount);
+    return new PreparedResultSet(mysqlStmt->GetSTMT(), result, rowCount, fieldCount);
 }
 
 bool MySQLConnection::_HandleMySQLErrno(uint32 errNo, uint8 attempts /*= 5*/)

src/server/database/Database/MySQLConnection.h
@@ -71,7 +71,7 @@ class TC_DATABASE_API MySQLConnection
         ResultSet* Query(char const* sql);
         PreparedResultSet* Query(PreparedStatement* stmt);
         bool _Query(char const* sql, MySQLResult** pResult, MySQLField** pFields, uint64* pRowCount, uint32* pFieldCount);
-        bool _Query(PreparedStatement* stmt, MySQLResult** pResult, uint64* pRowCount, uint32* pFieldCount);
+        bool _Query(PreparedStatement* stmt, MySQLPreparedStatement** mysqlStmt, MySQLResult** pResult, uint64* pRowCount, uint32* pFieldCount);
 
         void BeginTransaction();
         void RollbackTransaction();

src/server/database/Database/MySQLPreparedStatement.cpp
@@ -48,6 +48,66 @@ MySQLPreparedStatement::~MySQLPreparedStatement()
     delete[] m_bind;
 }
 
+void MySQLPreparedStatement::BindParameters(PreparedStatement* stmt)
+{
+    m_stmt = stmt;     // Cross reference them for debug output
+
+    uint8 pos = 0;
+    for (PreparedStatementData const& data : stmt->GetParameters())
+    {
+        switch (data.type)
+        {
+            case TYPE_BOOL:
+                setBool(pos, std::get<bool>(data.data));
+                break;
+            case TYPE_UI8:
+                setUInt8(pos, std::get<uint8>(data.data));
+                break;
+            case TYPE_UI16:
+                setUInt16(pos, std::get<uint16>(data.data));
+                break;
+            case TYPE_UI32:
+                setUInt32(pos, std::get<uint32>(data.data));
+                break;
+            case TYPE_I8:
+                setInt8(pos, std::get<int8>(data.data));
+                break;
+            case TYPE_I16:
+                setInt16(pos, std::get<int16>(data.data));
+                break;
+            case TYPE_I32:
+                setInt32(pos, std::get<int32>(data.data));
+                break;
+            case TYPE_UI64:
+                setUInt64(pos, std::get<uint64>(data.data));
+                break;
+            case TYPE_I64:
+                setInt64(pos, std::get<int64>(data.data));
+                break;
+            case TYPE_FLOAT:
+                setFloat(pos, std::get<float>(data.data));
+                break;
+            case TYPE_DOUBLE:
+                setDouble(pos, std::get<double>(data.data));
+                break;
+            case TYPE_STRING:
+                setString(pos, std::get<std::string>(data.data));
+                break;
+            case TYPE_BINARY:
+                setBinary(pos, std::get<std::vector<uint8>>(data.data));
+                break;
+            case TYPE_NULL:
+                setNull(pos);
+                break;
+        }
+        ++pos;
+    }
+#ifdef _DEBUG
+    if (pos < m_paramCount)
+        TC_LOG_WARN("sql.sql", "[WARNING]: BindParameters() for statement %u did not bind all allocated parameters", stmt->GetIndex());
+#endif
+}
+
 void MySQLPreparedStatement::ClearParameters()
 {
     for (uint32 i=0; i < m_paramCount; ++i)
@@ -82,10 +142,10 @@ static void SetParameterValue(MYSQL_BIND* param, enum_field_types type, void con
 //- Bind on mysql level
 void MySQLPreparedStatement::AssertValidIndex(uint8 index)
 {
-    ASSERT(index < m_paramCount || ParamenterIndexAssertFail(m_stmt->m_index, index, m_paramCount));
+    ASSERT(index < m_paramCount || ParamenterIndexAssertFail(m_stmt->GetIndex(), index, m_paramCount));
 
     if (m_paramsSet[index])
-        TC_LOG_ERROR("sql.sql", "[ERROR] Prepared Statement (id: %u) trying to bind value on already bound index (%u).", m_stmt->m_index, index);
+        TC_LOG_ERROR("sql.sql", "[ERROR] Prepared Statement (id: %u) trying to bind value on already bound index (%u).", m_stmt->GetIndex(), index);
 }
 
 void MySQLPreparedStatement::setNull(const uint8 index)
@@ -226,7 +286,7 @@ std::string MySQLPreparedStatement::getQueryString() const
     std::string queryString(m_queryString);
 
     size_t pos = 0;
-    for (PreparedStatementData& data : m_stmt->statement_data)
+    for (PreparedStatementData const& data : m_stmt->GetParameters())
     {
         pos = queryString.find('?', pos);
         std::stringstream ss;

src/server/database/Database/MySQLPreparedStatement.h
@@ -39,6 +39,8 @@ class TC_DATABASE_API MySQLPreparedStatement
         MySQLPreparedStatement(MySQLStmt* stmt, std::string queryString);
         ~MySQLPreparedStatement();
 
+        void BindParameters(PreparedStatement* stmt);
+
         void setNull(const uint8 index);
         void setBool(const uint8 index, const bool value);
         void setUInt8(const uint8 index, const uint8 value);

Szauron665/AngelCore

(cherry picked from commit 5b0a32d164ba91207360d8b8a266b69dd0db9869)

src/server/database/Database/MySQLConnection.cpp
@@ -198,13 +198,12 @@ bool MySQLConnection::Execute(PreparedStatementBase* stmt)
     if (!m_Mysql)
         return false;
 
-    uint32 index = stmt->m_index;
+    uint32 index = stmt->GetIndex();
 
     MySQLPreparedStatement* m_mStmt = GetPreparedStatement(index);
     ASSERT(m_mStmt);            // Can only be null if preparation failed, server side error or bad query
-    m_mStmt->m_stmt = stmt;     // Cross reference them for debug output
 
-    stmt->BindParameters(m_mStmt);
+    m_mStmt->BindParameters(stmt);
 
     MYSQL_STMT* msql_STMT = m_mStmt->GetSTMT();
     MYSQL_BIND* msql_BIND = m_mStmt->GetBind();
@@ -241,18 +240,18 @@ bool MySQLConnection::Execute(PreparedStatementBase* stmt)
     return true;
 }
 
-bool MySQLConnection::_Query(PreparedStatementBase* stmt, MySQLResult** pResult, uint64* pRowCount, uint32* pFieldCount)
+bool MySQLConnection::_Query(PreparedStatementBase* stmt, MySQLPreparedStatement** mysqlStmt, MySQLResult** pResult, uint64* pRowCount, uint32* pFieldCount)
 {
     if (!m_Mysql)
         return false;
 
-    uint32 index = stmt->m_index;
+    uint32 index = stmt->GetIndex();
 
     MySQLPreparedStatement* m_mStmt = GetPreparedStatement(index);
     ASSERT(m_mStmt);            // Can only be null if preparation failed, server side error or bad query
-    m_mStmt->m_stmt = stmt;     // Cross reference them for debug output
 
-    stmt->BindParameters(m_mStmt);
+    m_mStmt->BindParameters(stmt);
+    *mysqlStmt = m_mStmt;
 
     MYSQL_STMT* msql_STMT = m_mStmt->GetSTMT();
     MYSQL_BIND* msql_BIND = m_mStmt->GetBind();
@@ -265,7 +264,7 @@ bool MySQLConnection::_Query(PreparedStatementBase* stmt, MySQLResult** pResult,
         TC_LOG_ERROR("sql.sql", "SQL(p): %s\n [ERROR]: [%u] %s", m_mStmt->getQueryString().c_str(), lErrno, mysql_stmt_error(msql_STMT));
 
         if (_HandleMySQLErrno(lErrno))  // If it returns true, an error was handled successfully (i.e. reconnection)
-            return _Query(stmt, pResult, pRowCount, pFieldCount);       // Try again
+            return _Query(stmt, mysqlStmt, pResult, pRowCount, pFieldCount);       // Try again
 
         m_mStmt->ClearParameters();
         return false;
@@ -278,7 +277,7 @@ bool MySQLConnection::_Query(PreparedStatementBase* stmt, MySQLResult** pResult,
             m_mStmt->getQueryString().c_str(), lErrno, mysql_stmt_error(msql_STMT));
 
         if (_HandleMySQLErrno(lErrno))  // If it returns true, an error was handled successfully (i.e. reconnection)
-            return _Query(stmt, pResult, pRowCount, pFieldCount);      // Try again
+            return _Query(stmt, mysqlStmt, pResult, pRowCount, pFieldCount);      // Try again
 
         m_mStmt->ClearParameters();
         return false;
@@ -494,18 +493,19 @@ void MySQLConnection::PrepareStatement(uint32 index, std::string const& sql, Con
 
 PreparedResultSet* MySQLConnection::Query(PreparedStatementBase* stmt)
 {
+    MySQLPreparedStatement* mysqlStmt = nullptr;
     MySQLResult* result = nullptr;
     uint64 rowCount = 0;
     uint32 fieldCount = 0;
 
-    if (!_Query(stmt, &result, &rowCount, &fieldCount))
+    if (!_Query(stmt, &mysqlStmt, &result, &rowCount, &fieldCount))
         return nullptr;
 
     if (mysql_more_results(m_Mysql))
     {
         mysql_next_result(m_Mysql);
     }
-    return new PreparedResultSet(stmt->m_stmt->GetSTMT(), result, rowCount, fieldCount);
+    return new PreparedResultSet(mysqlStmt->GetSTMT(), result, rowCount, fieldCount);
 }
 
 bool MySQLConnection::_HandleMySQLErrno(uint32 errNo, uint8 attempts /*= 5*/)

src/server/database/Database/MySQLConnection.h
@@ -71,7 +71,7 @@ class TC_DATABASE_API MySQLConnection
         ResultSet* Query(char const* sql);
         PreparedResultSet* Query(PreparedStatementBase* stmt);
         bool _Query(char const* sql, MySQLResult** pResult, MySQLField** pFields, uint64* pRowCount, uint32* pFieldCount);
-        bool _Query(PreparedStatementBase* stmt, MySQLResult** pResult, uint64* pRowCount, uint32* pFieldCount);
+        bool _Query(PreparedStatementBase* stmt, MySQLPreparedStatement** mysqlStmt, MySQLResult** pResult, uint64* pRowCount, uint32* pFieldCount);
 
         void BeginTransaction();
         void RollbackTransaction();

src/server/database/Database/MySQLPreparedStatement.cpp
@@ -48,6 +48,66 @@ MySQLPreparedStatement::~MySQLPreparedStatement()
     delete[] m_bind;
 }
 
+void MySQLPreparedStatement::BindParameters(PreparedStatementBase* stmt)
+{
+    m_stmt = stmt;     // Cross reference them for debug output
+
+    uint8 pos = 0;
+    for (PreparedStatementData const& data : stmt->GetParameters())
+    {
+        switch (data.type)
+        {
+            case TYPE_BOOL:
+                setBool(pos, std::get<bool>(data.data));
+                break;
+            case TYPE_UI8:
+                setUInt8(pos, std::get<uint8>(data.data));
+                break;
+            case TYPE_UI16:
+                setUInt16(pos, std::get<uint16>(data.data));
+                break;
+            case TYPE_UI32:
+                setUInt32(pos, std::get<uint32>(data.data));
+                break;
+            case TYPE_I8:
+                setInt8(pos, std::get<int8>(data.data));
+                break;
+            case TYPE_I16:
+                setInt16(pos, std::get<int16>(data.data));
+                break;
+            case TYPE_I32:
+                setInt32(pos, std::get<int32>(data.data));
+                break;
+            case TYPE_UI64:
+                setUInt64(pos, std::get<uint64>(data.data));
+                break;
+            case TYPE_I64:
+                setInt64(pos, std::get<int64>(data.data));
+                break;
+            case TYPE_FLOAT:
+                setFloat(pos, std::get<float>(data.data));
+                break;
+            case TYPE_DOUBLE:
+                setDouble(pos, std::get<double>(data.data));
+                break;
+            case TYPE_STRING:
+                setString(pos, std::get<std::string>(data.data));
+                break;
+            case TYPE_BINARY:
+                setBinary(pos, std::get<std::vector<uint8>>(data.data));
+                break;
+            case TYPE_NULL:
+                setNull(pos);
+                break;
+        }
+        ++pos;
+    }
+#ifdef _DEBUG
+    if (pos < m_paramCount)
+        TC_LOG_WARN("sql.sql", "[WARNING]: BindParameters() for statement %u did not bind all allocated parameters", stmt->GetIndex());
+#endif
+}
+
 void MySQLPreparedStatement::ClearParameters()
 {
     for (uint32 i=0; i < m_paramCount; ++i)
@@ -82,10 +142,10 @@ static void SetParameterValue(MYSQL_BIND* param, enum_field_types type, const vo
 //- Bind on mysql level
 void MySQLPreparedStatement::AssertValidIndex(uint8 index)
 {
-    ASSERT(index < m_paramCount || ParamenterIndexAssertFail(m_stmt->m_index, index, m_paramCount));
+    ASSERT(index < m_paramCount || ParamenterIndexAssertFail(m_stmt->GetIndex(), index, m_paramCount));
 
     if (m_paramsSet[index])
-        TC_LOG_ERROR("sql.sql", "[ERROR] Prepared Statement (id: %u) trying to bind value on already bound index (%u).", m_stmt->m_index, index);
+        TC_LOG_ERROR("sql.sql", "[ERROR] Prepared Statement (id: %u) trying to bind value on already bound index (%u).", m_stmt->GetIndex(), index);
 }
 
 void MySQLPreparedStatement::setNull(const uint8 index)
@@ -226,7 +286,7 @@ std::string MySQLPreparedStatement::getQueryString() const
     std::string queryString(m_queryString);
 
     size_t pos = 0;
-    for (PreparedStatementData& data : m_stmt->statement_data)
+    for (PreparedStatementData const& data : m_stmt->GetParameters())
     {
         pos = queryString.find('?', pos);
         std::stringstream ss;

src/server/database/Database/MySQLPreparedStatement.h
@@ -39,6 +39,8 @@ class TC_DATABASE_API MySQLPreparedStatement
         MySQLPreparedStatement(MySQLStmt* stmt, std::string queryString);
         ~MySQLPreparedStatement();
 
+        void BindParameters(PreparedStatementBase* stmt);
+
         void setNull(const uint8 index);
         void setBool(const uint8 index, const bool value);
         void setUInt8(const uint8 index, const uint8 value);