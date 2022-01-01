allankrdec/TrinityCore-10-2024

Scripts/Naxxramas: Fix Heigan Eruption spell (#24470)

src/server/scripts/Northrend/Naxxramas/boss_heigan.cpp
@@ -30,8 +30,7 @@ enum Spells
     SPELL_DECREPIT_FEVER    = 29998, // 25-man: 55011
     SPELL_SPELL_DISRUPTION  = 29310,
     SPELL_PLAGUE_CLOUD      = 29350,
-    SPELL_TELEPORT_SELF     = 30211,
-    SPELL_ERUPTION          = 29371
+    SPELL_TELEPORT_SELF     = 30211
 };
 
 enum Yells
@@ -194,7 +193,9 @@ public:
                                     if (GameObject* tile = ObjectAccessor::GetGameObject(*me, tileGUID))
                                     {
                                         tile->SendCustomAnim(0);
-                                        tile->CastSpell(nullptr, SPELL_ERUPTION);
+                                        CastSpellExtraArgs args;
+                                        args.OriginalCaster = me->GetGUID();
+                                        tile->CastSpell(tile, tile->GetGOInfo()->trap.spellId, args);
                                     }
 
                         if (_safeSection == 0)

Szauron665/AngelCore

(cherry picked from commit 6c7c2c6cd279813333e0b1cc1f60b22a04f368dd)

src/server/scripts/Northrend/Naxxramas/boss_heigan.cpp
@@ -29,8 +29,7 @@ enum Spells
     SPELL_DECREPIT_FEVER    = 29998, // 25-man: 55011
     SPELL_SPELL_DISRUPTION  = 29310,
     SPELL_PLAGUE_CLOUD      = 29350,
-    SPELL_TELEPORT_SELF     = 30211,
-    SPELL_ERUPTION          = 29371
+    SPELL_TELEPORT_SELF     = 30211
 };
 
 enum Yells
@@ -193,7 +192,9 @@ public:
                                     if (GameObject* tile = ObjectAccessor::GetGameObject(*me, tileGUID))
                                     {
                                         tile->SendCustomAnim(0);
-                                        tile->CastSpell(nullptr, SPELL_ERUPTION);
+                                        CastSpellExtraArgs args;
+                                        args.OriginalCaster = me->GetGUID();
+                                        tile->CastSpell(tile, tile->GetGOInfo()->trap.spell, args);
                                     }
 
                         if (_safeSection == 0)