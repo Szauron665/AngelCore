allankrdec/TrinityCore-10-2024

Core/Chat: Correct Hyperlink validation for incomplete achievements.

src/server/game/Chat/Hyperlinks.h
@@ -20,6 +20,7 @@
 
 #include "ObjectGuid.h"
 #include "StringConvert.h"
+#include <array>
 #include <string>
 #include <string_view>
 #include <type_traits>
@@ -41,10 +42,10 @@ namespace Trinity::Hyperlinks
         AchievementEntry const* Achievement;
         ObjectGuid::LowType CharacterId;
         bool IsFinished;
-        uint16 Year;
+        uint8 Year;
         uint8 Month;
         uint8 Day;
-        uint32 Criteria[4];
+        std::array<uint32, 4> Criteria;
     };
 
     struct GlyphLinkData
@@ -57,7 +58,7 @@ namespace Trinity::Hyperlinks
     {
         ItemTemplate const* Item;
         uint32 EnchantId;
-        uint32 GemEnchantId[3];
+        std::array<uint32, 3> GemEnchantId;
         int32 RandomPropertyId;
         int32 RandomPropertySeed;
         uint8 RenderLevel;

Szauron665/AngelCore

(cherry picked from commit fdcb1c8b2e142acc012f2ecb2ece082302e8fb15)

src/server/game/Chat/Hyperlinks.h
@@ -20,6 +20,7 @@
 
 #include "ObjectGuid.h"
 #include "StringConvert.h"
+#include <array>
 #include <string>
 #include <string_view>
 #include <type_traits>
@@ -60,10 +61,10 @@ namespace Trinity::Hyperlinks
         AchievementEntry const* Achievement;
         ObjectGuid CharacterId;
         bool IsFinished;
-        uint16 Year;
+        uint8 Year;
         uint8 Month;
         uint8 Day;
-        uint32 Criteria[4];
+        std::array<uint32, 4> Criteria;
     };
 
     struct ArtifactPowerLinkData
@@ -128,7 +129,7 @@ namespace Trinity::Hyperlinks
     {
         ItemTemplate const* Item;
         uint32 EnchantId;
-        uint32 GemItemId[3];
+        std::array<uint32, 3> GemItemId;
         uint8 RenderLevel;
         uint32 RenderSpecialization;
         uint8 Context;