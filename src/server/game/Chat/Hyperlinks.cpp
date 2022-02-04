allankrdec/TrinityCore-10-2024

UnitTests: Add a first set of item hyperlink tests

src/server/game/Chat/Hyperlinks.cpp
@@ -152,7 +152,7 @@ struct LinkValidator<LinkTags::item>
         {
             if (!locale && i != DEFAULT_LOCALE)
                 continue;
-            std::string const& name = (i == DEFAULT_LOCALE) ? data.Item->Name1 : locale->Name[i];
+            std::string_view name = (i == DEFAULT_LOCALE) ? data.Item->Name1 : ObjectMgr::GetLocaleString(locale->Name, i);
             if (name.empty())
                 continue;
             if (randomSuffixes)
@@ -184,15 +184,17 @@ struct LinkValidator<LinkTags::quest>
     static bool IsTextValid(QuestLinkData const& data, std::string_view text)
     {
         QuestLocale const* locale = sObjectMgr->GetQuestLocale(data.Quest->GetQuestId());
-        if (!locale)
-            return text == data.Quest->GetTitle();
+
+        if (text == data.Quest->GetTitle())
+            return true;
 
         for (uint8 i = 0; i < TOTAL_LOCALES; ++i)
         {
-            std::string const& name = (i == DEFAULT_LOCALE) ? data.Quest->GetTitle() : locale->Title[i];
-            if (name.empty())
+            if (i == DEFAULT_LOCALE)
                 continue;
-            if (text == name)
+
+            std::string_view name = ObjectMgr::GetLocaleString(locale->Title, i);
+            if (!name.empty() && (text == name))
                 return true;
         }
 

Szauron665/AngelCore

(cherry picked from commit 5394b2ef0f1c7b48e8886257b93698358abb575f)

src/server/game/Chat/Hyperlinks.cpp
@@ -372,7 +372,7 @@ struct LinkValidator<LinkTags::item>
     {
         for (LocaleConstant i = LOCALE_enUS; i < TOTAL_LOCALES; i = LocaleConstant(i + 1))
         {
-            std::string name = itemTemplate->GetName(i);
+            std::string_view name = itemTemplate->GetName(i);
             if (name.empty())
                 continue;
             if (suffixStrings)
@@ -449,16 +449,20 @@ struct LinkValidator<LinkTags::quest>
 {
     static bool IsTextValid(QuestLinkData const& data, std::string_view text)
     {
+        if (text == data.Quest->GetLogTitle())
+            return true;
+
         QuestTemplateLocale const* locale = sObjectMgr->GetQuestLocale(data.Quest->GetQuestId());
         if (!locale)
-            return data.Quest->GetLogTitle().c_str() == text;
+            return false;
 
         for (uint8 i = 0; i < TOTAL_LOCALES; ++i)
         {
-            std::string const& name = (i == DEFAULT_LOCALE) ? data.Quest->GetLogTitle() : locale->LogTitle[i];
-            if (name.empty())
+            if (i == DEFAULT_LOCALE)
                 continue;
-            if (text == name)
+
+            std::string_view name = ObjectMgr::GetLocaleString(locale->LogTitle, i);
+            if (!name.empty() && (text == name))
                 return true;
         }
 