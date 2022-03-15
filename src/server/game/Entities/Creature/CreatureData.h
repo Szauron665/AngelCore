allankrdec/TrinityCore-10-2024

Core/Misc: Link CREATURE_FLAG_EXTRA_INSTANCE_BIND to corresponding static flag

src/server/game/Entities/Creature/CreatureData.h
@@ -71,7 +71,7 @@ enum CreatureStaticFlags2
 {
     CREATURE_STATIC_FLAG_2_NO_PET_SCALING                  = 0x00000001,
     CREATURE_STATIC_FLAG_2_FORCE_PARTY_MEMBERS_INTO_COMBAT = 0x00000002, // Original description: Force Raid Combat
-    CREATURE_STATIC_FLAG_2_LOCK_TAPPERS_TO_RAID_ON_DEATH   = 0x00000004,
+    CREATURE_STATIC_FLAG_2_RAID_LOCK_ON_DEATH              = 0x00000004, // "Lock Tappers To Raid On Death", toggleable by 'Set "RAID_LOCK_ON_DEATH" flag for unit(s)' action, CREATURE_FLAG_EXTRA_INSTANCE_BIND
     CREATURE_STATIC_FLAG_2_SPELL_ATTACKABLE                = 0x00000008, // CREATURE_TYPE_FLAG_SPELL_ATTACKABLE, original description(not valid anymore?): No Harmful Vertex Coloring
     CREATURE_STATIC_FLAG_2_NO_CRUSHING_BLOWS               = 0x00000010, // CREATURE_FLAG_EXTRA_NO_CRUSHING_BLOWS
     CREATURE_STATIC_FLAG_2_NO_OWNER_THREAT                 = 0x00000020,

Szauron665/AngelCore

(cherry picked from commit f89bb434196c045b684218f5e7a10a6ffd5cd421)

src/server/game/Entities/Creature/CreatureData.h
@@ -71,7 +71,7 @@ enum CreatureStaticFlags2
 {
     CREATURE_STATIC_FLAG_2_NO_PET_SCALING                  = 0x00000001,
     CREATURE_STATIC_FLAG_2_FORCE_PARTY_MEMBERS_INTO_COMBAT = 0x00000002, // Original description: Force Raid Combat
-    CREATURE_STATIC_FLAG_2_LOCK_TAPPERS_TO_RAID_ON_DEATH   = 0x00000004,
+    CREATURE_STATIC_FLAG_2_LOCK_TAPPERS_TO_RAID_ON_DEATH   = 0x00000004, // "Lock Tappers To Raid On Death", toggleable by 'Set "RAID_LOCK_ON_DEATH" flag for unit(s)' action, CREATURE_FLAG_EXTRA_INSTANCE_BIND
     CREATURE_STATIC_FLAG_2_SPELL_ATTACKABLE                = 0x00000008, // CREATURE_TYPE_FLAG_SPELL_ATTACKABLE, original description(not valid anymore?): No Harmful Vertex Coloring
     CREATURE_STATIC_FLAG_2_NO_CRUSHING_BLOWS               = 0x00000010, // CREATURE_FLAG_EXTRA_NO_CRUSHING_BLOWS
     CREATURE_STATIC_FLAG_2_NO_OWNER_THREAT                 = 0x00000020,